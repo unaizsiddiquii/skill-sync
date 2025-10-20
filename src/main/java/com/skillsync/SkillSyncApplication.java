686d36a0df33ffd0a06b56afb9d03d22b02f7896
Unaiz Siddiqui <unaiz4321@gmail.com>
2025-10-20 15:52:50 +0530

    Initial commit: SkillSync base structure

---
+++ src/main/java/com/skillsync/SkillSyncApplication.java
@@ -0,0 +1,13 @@
+package com.skillsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillsync/SkillSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
